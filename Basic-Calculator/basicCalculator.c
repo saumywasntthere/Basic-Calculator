3340b397019d57aa5596f796e39d0a4b6084a3ad
Saumy Kakkad <83053263+saumywasntthere@users.noreply.github.com>
2024-09-19 15:17:08 +0530

    Update basicCalculator.c

--- Basic-Calculator/basicCalculator.c
+++ Basic-Calculator/basicCalculator.c
@@ -13,18 +13,66 @@ int main()
     float num2;
     float result;
 
-    printf("Enter the first number: ");
-    scanf("%f", &num1);
-    do
+    while(1)
     {
-        printf("Enter the second number \(not 0\): ");
-        scanf("%f", &num2);
+        printf("Enter the first number: ");
+        if(scanf("%f", &num1) == 1)
+        {
+            break;
+        }
+        else
+        {
+            printf("This is not a number. Please enter a number.\n");
+            while(getchar() != '\n');
+        }
     }
-    while (num2 == 0);
+
     getchar();
 
-    printf("Choose a function \(+, -, *, /\): ");
-    scanf("%c", &function);
+    while(1)
+    {
+        printf("Choose a function (+, -, *, /): ");
+        if(scanf("%c", &function) == 1 && (function == '+' || function == '-' || function == '*' || function == '/'))
+        {
+            break;
+        }
+        else
+        {
+            printf("Invalid function. Please choose +, -, * or /.\n");
+            while(getchar() != '\n');
+        }
+    }
+
+    if(function == '/')
+    {
+        do
+        {
+            while(1)
+            {
+                printf("Enter the second number (not 0): ");
+                if(scanf("%f", &num2) == 1 && num2 != 0)
+                {
+                    break;
+                }
+                else
+                {
+                    printf("This is not an accepted value. Please enter a number greater than 0.\n");
+                    while(getchar() != '\n');
+                }
+            }
+        }
+        while (num2 == 0);
+    }
+    else
+    {
+        printf("Enter the second number: ");
+        while(scanf("%f", &num2) != 1)
+        {
+            printf("Invalid input. \nPlease enter a valid number: ");
+            while(getchar() != '\n');
+        }
+    }
+
     printf("%f %c %f = ", num1, function, num2);
 
     if(function == '+')
@@ -43,12 +91,8 @@ int main()
     {
         result = divide(num1, num2);
     }
-    else
-    {
-        printf("Invalid function.");
-    }
 
-    printf("%f", result);
+    printf("%f\n", result);
     return 0;
 }
 
